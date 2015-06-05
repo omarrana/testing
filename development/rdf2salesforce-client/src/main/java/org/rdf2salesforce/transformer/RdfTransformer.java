3a71cf9e26d2608e8de2b6e7aa62bec144067bd3
Melnyk, Alexander <amelnyk@eitco.de>
2015-06-05 10:30:48 +0200

    added transformer package and class, so Mincho can start working on it

---
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/transformer/RdfTransformer.java
@@ -0,0 +1,20 @@
+package org.rdf2salesforce.transformer;
+
+import org.openrdf.model.Literal;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.ValueFactoryImpl;
+
+public class RdfTransformer {
+	
+	// just some testing
+	public static void transform(){
+		ValueFactoryImpl factory = new ValueFactoryImpl();
+		URI bob = factory.createURI("http://example.org/bob");
+		URI name = factory.createURI("http://example.org/name");
+		Literal bobsName = factory.createLiteral("Bob");
+		Statement nameStatement = factory.createStatement(bob, name, bobsName);
+		
+	}
+
+}
