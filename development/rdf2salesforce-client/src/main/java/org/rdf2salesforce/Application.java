2a6c0cf34b2612215455395ce5cc65c1eff3fb30
Alexander Melnyk <a.schroet@gmail.com>
2015-06-19 01:02:10 +0200

    contact has now the most important fields of the salesforce sobject "Contact"

--- development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/Application.java
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/Application.java
@@ -4,6 +4,13 @@ import org.rdf2salesforce.services.LoginService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 
@@ -15,6 +22,17 @@ public class Application {
     	LoginService loginService = ctx.getBean(LoginService.class);
     	AccessToken token = loginService.getToken().getBody();
     	System.out.println(token);
+    	RestTemplate restTemplate = new RestTemplate();
+		UriComponentsBuilder builder = UriComponentsBuilder
+				.fromHttpUrl(token.getInstanceUrl()+ "/services/data/v34.0/sobjects/Contact/describe");
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		headers.set("Authorization", token.getTokenType()+ " " + token.getAccessToken());
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		ResponseEntity<String> exchange = restTemplate.exchange(builder
+				.build().encode().toUri(), HttpMethod.GET, entity,
+				String.class);
+    	System.out.println(exchange.getBody());
     }
 
 }
--- development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/model/Contact.java
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/model/Contact.java
@@ -1,29 +1,141 @@
 package org.rdf2salesforce.model;
 
+import java.util.Date;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Contact {
 
-    private String name;
-    private String about;
-    private String phone;
-    private String website;
+	private String accountId;
+	private String department;
+	private String description;
+	private String name;
+	private Date birthday;
+    private String firstName;
+	private String lastName;
+	private String mailingCity;
+	private String mailingStreet;
+	private String mailingState;
+	private String mailingCountry;
+	private String mailingPostalCode;
+	private String mailingCountyCode;
+	private String addressZip;
+	private String phone;
+	private String fax;
+	private String mobilePhone;
+	private String email;
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMailingCity() {
+		return mailingCity;
+	}
+	public void setMailingCity(String mailingCity) {
+		this.mailingCity = mailingCity;
+	}
+	public String getMailingStreet() {
+		return mailingStreet;
+	}
+	public void setMailingStreet(String mailingStreet) {
+		this.mailingStreet = mailingStreet;
+	}
+	public String getMailingState() {
+		return mailingState;
+	}
+	public void setMailingState(String mailingState) {
+		this.mailingState = mailingState;
+	}
+	public String getMailingCountry() {
+		return mailingCountry;
+	}
+	public void setMailingCountry(String mailingCountry) {
+		this.mailingCountry = mailingCountry;
+	}
+	public String getMailingPostalCode() {
+		return mailingPostalCode;
+	}
+	public void setMailingPostalCode(String mailingPostalCode) {
+		this.mailingPostalCode = mailingPostalCode;
+	}
+	public String getMailingCountyCode() {
+		return mailingCountyCode;
+	}
+	public void setMailingCountyCode(String mailingCountyCode) {
+		this.mailingCountyCode = mailingCountyCode;
+	}
+	public String getAddressZip() {
+		return addressZip;
+	}
+	public void setAddressZip(String addressZip) {
+		this.addressZip = addressZip;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
     
-    public String getName() {
-        return name;
-    }
-    
-    public String getAbout() {
-        return about;
-    }
-    
-    public String getPhone() {
-        return phone;
-    }
-    
-    public String getWebsite() {
-        return website;
-    }
+
+
     
 }
