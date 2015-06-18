cea29ff10326fdf493723b1f60700e5bbe155def
Alexander Melnyk <a.schroet@gmail.com>
2015-06-19 01:13:39 +0200

    service calls for contacts are now in ContactService. We can now get contact information from salesforce

--- development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/Application.java
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/Application.java
@@ -1,16 +1,13 @@
 package org.rdf2salesforce;
 
+import java.util.List;
+
+import org.rdf2salesforce.model.Contact;
+import org.rdf2salesforce.services.ContactService;
 import org.rdf2salesforce.services.LoginService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 
 
@@ -22,17 +19,10 @@ public class Application {
     	LoginService loginService = ctx.getBean(LoginService.class);
     	AccessToken token = loginService.getToken().getBody();
     	System.out.println(token);
-    	RestTemplate restTemplate = new RestTemplate();
-		UriComponentsBuilder builder = UriComponentsBuilder
-				.fromHttpUrl(token.getInstanceUrl()+ "/services/data/v34.0/sobjects/Contact/describe");
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-		headers.set("Authorization", token.getTokenType()+ " " + token.getAccessToken());
-		HttpEntity<?> entity = new HttpEntity<>(headers);
-		ResponseEntity<String> exchange = restTemplate.exchange(builder
-				.build().encode().toUri(), HttpMethod.GET, entity,
-				String.class);
-    	System.out.println(exchange.getBody());
+    	ContactService contactService = ctx.getBean(ContactService.class);
+    	String allContacts = contactService.getAll(token);
+    	System.out.println(allContacts);
+    	
     }
 
 }
