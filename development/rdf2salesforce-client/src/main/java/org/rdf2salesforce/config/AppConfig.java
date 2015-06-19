6aacfddf790fdb2ac27c10a3c814c75de93479c1
Alexander Melnyk <a.schroet@gmail.com>
2015-06-20 00:47:21 +0200

    forgot to add the config class previously

---
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/config/AppConfig.java
@@ -0,0 +1,24 @@
+package org.rdf2salesforce.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+	
+	@Value("${client.id}")
+	public String CLIENT_ID;
+	@Value("${client.secret}")
+	public String CLIENT_SECRET;
+	@Value("${client.username}")
+	public String USERNAME;
+	@Value("${client.password}")
+	public String PASSWORD;
+	@Value("${login_url}")
+	public String LOGIN_URL;
+	
+	
+	
+
+}
