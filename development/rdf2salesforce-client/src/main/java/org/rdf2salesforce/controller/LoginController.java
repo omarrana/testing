dd09c0e854992b1d6866eef21a1febc39fd3347d
Alexander Melnik <a.schroet@gmail.com>
2015-09-22 16:02:24 +0200

    we can now login

--- development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/controller/LoginController.java
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/controller/LoginController.java
@@ -1,5 +1,26 @@
 package org.rdf2salesforce.controller;
 
-public class LoginController {
+import org.rdf2salesforce.AccessToken;
+import org.rdf2salesforce.model.Contact;
+import org.rdf2salesforce.services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class LoginController {
+	@Autowired
+	LoginService loginService;
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String getContact(@RequestParam(value = "clientId") String clientId,
+			@RequestParam(value = "clientSecret") String clientSecret,
+			@RequestParam(value = "clientUsername") String clientUsername,
+			@RequestParam(value = "clientPassword") String clientPassword) {
+		AccessToken token = loginService.login(clientId, clientSecret,
+				clientUsername, clientPassword);
+		return token.getAccessToken();
+	}
 }
