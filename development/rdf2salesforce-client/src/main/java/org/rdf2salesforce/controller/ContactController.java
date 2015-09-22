c60d4da4c7f4b6a5d3f57d13ff14c31af39b7f34
Alexander Melnik <a.schroet@gmail.com>
2015-09-22 18:43:59 +0200

    refactored pathVariable and added RequestMapping for RDF output

--- development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/controller/ContactController.java
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/controller/ContactController.java
@@ -5,31 +5,37 @@ import java.util.List;
 import org.rdf2salesforce.model.Contact;
 import org.rdf2salesforce.services.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/contact")
 public class ContactController {
 
 	@Autowired
 	ContactService contactService;
 
-	@RequestMapping(value = "/contact/{id}", method = RequestMethod.GET)
-	public Contact getContact(@RequestParam(value = "id") String id,
-							  @RequestParam(value = "token") String token,
-							  @RequestParam(value = "instance") String instance) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Contact getContact(@PathVariable(value = "id") String id,
+			@RequestParam(value = "token") String token,
+			@RequestParam(value = "instance") String instance) {
 		return contactService.getContact(id, token, instance);
 	}
-	
-	@RequestMapping(value = "/contact", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public List<Contact> getAll(@RequestParam(value = "token") String token,
-								@RequestParam(value = "instance") String instance) {
+			@RequestParam(value = "instance") String instance) {
 		return contactService.getAll(token, instance);
 	}
-	
-	
-	
+
+	@RequestMapping(value = "/rdf/{id}", method = RequestMethod.GET, produces={"application/xml"})
+	public String getContactAsRdf(@PathVariable(value = "id") String id,
+			@RequestParam(value = "token") String token,
+			@RequestParam(value = "instance") String instance) {
+		return contactService.getContact(id, token, instance).toRdf();
+	}
 
 }
