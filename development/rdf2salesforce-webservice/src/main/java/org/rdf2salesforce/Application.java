9d38c70b35ac383e052c0fcd8c8350487c6682da
Alexander Melnyk <a.schroet@googlemail.com>
2015-09-23 12:30:12 +0200

    removed old code and logger, we dont need no logger here...for now

--- development/rdf2salesforce-webservice/src/main/java/org/rdf2salesforce/Application.java
+++ development/rdf2salesforce-webservice/src/main/java/org/rdf2salesforce/Application.java
@@ -10,32 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	
-	private static Logger LOGGER = LoggerFactory
-			.getLogger(Application.class);
-
     public static void main(String args[]){
     	SpringApplication.run(Application.class, args);
-//    	ApplicationContext ctx = SpringApplication.run(Application.class, args);
-//    	LoginService loginService = ctx.getBean(LoginService.class);
-//    	String token = loginService.getToken().getAccessToken();
-//    	System.out.println(token);
-//    	ContactService contactService = ctx.getBean(ContactService.class);
-//    	List<Contact> allContacts = contactService.getAll(token);
-//    	allContacts.forEach(contact -> System.out.println(contact.getName()));
-//    	Contact newContact = new Contact();
-//    	newContact.setGivenName("Adam");
-//    	newContact.setFamilyName("Jenkins");
-//    	CreateResponse createResponse = contactService.createContact(newContact, token);
-//    	String newContactId = createResponse.getId();
-//    	newContact.setId(newContactId);
-//    	newContact.setGivenName("Jack");
-//    	newContact.setFamilyName("Prod");
-//    	newContact.setEmailAdress("jack@gmail.com");
-//    	contactService.updateContact(newContact, token);
-//    	contactService.deleteContact(newContact, token);
-//    	ArrayList<Contact> contactsFromRdf = contactService.createFromRdf();
-//    	contactsFromRdf.forEach(contact -> LOGGER.info(contact.toString()));
-//    	contactService.createRdfContact(newContact);
     	
     }
 
