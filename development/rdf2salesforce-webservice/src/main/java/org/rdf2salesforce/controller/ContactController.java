9cd2bb1fb9aeb67426175f61ad684211340547a4
Alexander Melnik <a.schroet@gmail.com>
2015-09-26 16:57:07 +0200

    updateContact

--- development/rdf2salesforce-webservice/src/main/java/org/rdf2salesforce/controller/ContactController.java
+++ development/rdf2salesforce-webservice/src/main/java/org/rdf2salesforce/controller/ContactController.java
@@ -54,5 +54,13 @@ public class ContactController {
 			@RequestParam(value = "instance") String instance) {
 		contactService.deleteContact(id, token, instance);
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
+	public String updateContact(@PathVariable(value = "id") String id,
+			@RequestParam(value = "token") String token,
+			@RequestParam(value = "instance") String instance,
+			@RequestBody Contact contact) {
+		return contactService.updateContact(contact, token, instance);
+	}
 
 }
