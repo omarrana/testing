51cd4dec407535c13b8c59f97ac72e851c9872ef
Alexander Melnik <a.schroet@gmail.com>
2015-09-28 23:28:06 +0200

    added some basic calls to the service

--- development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/Application.java
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/Application.java
@@ -1,5 +1,7 @@
 package org.rdf2salesforce;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,15 +9,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
-
+	private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String args[]) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-		Rdf2SalesforceService service = ctx.getBean(Rdf2SalesforceService.class);
+		ConfigurableApplicationContext ctx = SpringApplication.run(
+				Application.class, args);
+		Rdf2SalesforceService service = ctx
+				.getBean(Rdf2SalesforceService.class);
 		AccessToken login = service.login();
-		System.out.println(login.getAccessToken());
+		String instance = login.getInstanceUrl().replace("https://", "")
+				.split("\\.")[0];
+		String allContacts = service.getAll(login.getAccessToken(), instance);
+		LOGGER.info(allContacts);
+		String contact = service.getContact("0032400000BhDdzAAF", login.getAccessToken(), instance);
+		LOGGER.info(contact);
+		String rdfContact = service.getContactAsRdf("0032400000BhDdzAAF", login.getAccessToken(), instance);
+		LOGGER.info(rdfContact);
 	}
-
-	
 
 }
--- development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/Rdf2SalesforceService.java
+++ development/rdf2salesforce-client/src/main/java/org/rdf2salesforce/Rdf2SalesforceService.java
@@ -1,5 +1,7 @@
 package org.rdf2salesforce;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,10 +15,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class Rdf2SalesforceService {
-	
+
 	@Autowired
 	AppConfig appConfig;
-	
+
+	private static Logger LOGGER = LoggerFactory
+			.getLogger(Rdf2SalesforceService.class);
+
 	public AccessToken login() {
 		RestTemplate restTemplate = new RestTemplate();
 		UriComponentsBuilder builder = UriComponentsBuilder
@@ -31,6 +36,7 @@ public class Rdf2SalesforceService {
 		ResponseEntity<AccessToken> exchange = restTemplate.exchange(builder
 				.build().encode().toUri(), HttpMethod.POST, entity,
 				AccessToken.class);
+		LOGGER.info(exchange.toString());
 		return exchange.getBody();
 	}
 
@@ -46,6 +52,54 @@ public class Rdf2SalesforceService {
 		if (exchange.getStatusCode().equals(HttpStatus.OK)) {
 			// everything is fine, TODO: handle errors
 		}
+	}
+
+	public String getAll(String token, String instance) {
+		RestTemplate restTemplate = new RestTemplate();
+		UriComponentsBuilder builder = UriComponentsBuilder
+				.fromHttpUrl("http://localhost:8080/contact/")
+				.queryParam("token", token).queryParam("instance", instance);
+		HttpHeaders headers = createHeaders(token);
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		ResponseEntity<String> exchange = restTemplate.exchange(builder.build()
+				.encode().toUri(), HttpMethod.GET, entity, String.class);
+		return exchange.getBody();
+	}
+
+	public String getContact(String contactId, String token, String instance) {
+		ResponseEntity<String> exchange = null;
+		RestTemplate restTemplate = new RestTemplate();
+		UriComponentsBuilder builder = UriComponentsBuilder
+				.fromHttpUrl("http://localhost:8080/contact/" + contactId)
+				.queryParam("token", token).queryParam("instance", instance);
+		HttpHeaders headers = createHeaders(token);
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		exchange = restTemplate.exchange(builder.build().encode().toUri(),
+				HttpMethod.GET, entity, String.class);
+		return exchange.getBody();
+	}
+	
+	public String getContactAsRdf(String contactId, String token, String instance) {
+		ResponseEntity<String> exchange = null;
+		RestTemplate restTemplate = new RestTemplate();
+		UriComponentsBuilder builder = UriComponentsBuilder
+				.fromHttpUrl("http://localhost:8080/contact/rdf/" + contactId)
+				.queryParam("token", token).queryParam("instance", instance);
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_XML_VALUE);
+		headers.add("Content-Type", "application/xml");
+		headers.set("Authorization", "Bearer" + " " + token);
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		exchange = restTemplate.exchange(builder.build().encode().toUri(),
+				HttpMethod.GET, entity, String.class);
+		return exchange.getBody();
+	}
+
+	private HttpHeaders createHeaders(String token) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		headers.set("Authorization", "Bearer" + " " + token);
+		return headers;
 	}
 
 }
